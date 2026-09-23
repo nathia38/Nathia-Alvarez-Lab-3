756864c94560cdc18ceb64ac701e183047765839
nathia38 <nalvarez29@setoncatholic.org>
2026-09-23 20:14:39 +0000

    claculater

---
+++ main.py
@@ -0,0 +1,27 @@
+# this function adds two numbers
+def add(x,y):
+    print(x+y)
+
+add(6,7)
+
+# this function subtracts two numbers
+def add(x,y):
+    print(x-y)
+
+add(6,7)
+
+
+# this function divides two numbers
+def add(x,y):
+    print(x/y)
+
+add(6,7)
+
+
+# this function multiplies two numbers
+def add(x,y):
+    print(x*y)
+
+add(6,7)
+
+
