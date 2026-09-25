a7e13abf99165911009439eef242b855fb46e4d9
nathia38 <nalvarez29@setoncatholic.org>
2026-09-25 15:50:05 +0000

    third commit

--- main.py
+++ main.py
@@ -2,26 +2,56 @@
 def add(x,y):
     print(x+y)
 
-add(6,7)
 
 # this function subtracts two numbers
-def add(x,y):
+def subtract(x,y):
     print(x-y)
 
-add(6,7)
 
 
 # this function divides two numbers
-def add(x,y):
+def divide(x,y):
     print(x/y)
-
-add(6,7)
 
 
 # this function multiplies two numbers
-def add(x,y):
+def multiply(x,y):
     print(x*y)
 
-add(6,7)
 
+
+####start of program
+
+print("Welcome to my awesome calc app!!")
+print("What would you like to do?")
+print("Type (a)dd (s)ubtract (m)ultiply (d)ivide (q)uit")
+
+user_choice = input(":  ")
+#print(user_choice)
+
+if user_choice == 'a':
+    x = int(input("Enter the first number: "))
+    y = int(input("Enter the second number: "))
+    add(x,y)
+
+elif user_choice == 's':
+    x = int(input("Enter the first number"))
+    y = int(input("Enter the second number"))
+    subtract(x,y)
+
+elif user_choice == 'm':
+    x = int(input("Enter the first number"))
+    y = int(input("Enter the second number"))
+    multiply(x,y)
+
+elif user_choice == 'd':
+    x = int(input("Enter the first number"))
+    y = int(input("Enter the second number"))   
+    divide(x,y)
+
+elif user_choice == 'q':
+    print("Thanks for using my awesome calc app!!")
+
+else:
+    print("Invalid input. Please try again.")  
 
