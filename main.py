395df63cdad9dd538ac61673809d3c248cf5ffd3
nathia38 <nalvarez29@setoncatholic.org>
2026-09-25 15:50:18 +0000

    third commit

--- main.py
+++ main.py
@@ -22,36 +22,46 @@ def multiply(x,y):
 
 ####start of program
 
-print("Welcome to my awesome calc app!!")
-print("What would you like to do?")
-print("Type (a)dd (s)ubtract (m)ultiply (d)ivide (q)uit")
 
-user_choice = input(":  ")
+
+print("Welcome to my awesome calc app!!")
+while (True):
+
+    print("What would you like to do?")
+    print("Type (a)dd (s)ubtract (m)ultiply (d)ivide (q)uit")
+
+    user_choice = input(":  ")
 #print(user_choice)
 
-if user_choice == 'a':
-    x = int(input("Enter the first number: "))
-    y = int(input("Enter the second number: "))
-    add(x,y)
 
-elif user_choice == 's':
-    x = int(input("Enter the first number"))
-    y = int(input("Enter the second number"))
-    subtract(x,y)
+    
 
-elif user_choice == 'm':
-    x = int(input("Enter the first number"))
-    y = int(input("Enter the second number"))
-    multiply(x,y)
+    if user_choice == 'a':
+        x = int(input("Enter the first number: "))
+        y = int(input("Enter the second number: "))
+        add(x,y)
 
-elif user_choice == 'd':
-    x = int(input("Enter the first number"))
-    y = int(input("Enter the second number"))   
-    divide(x,y)
+    elif user_choice == 's':
+        x = int(input("Enter the first number: "))
+        y = int(input("Enter the second number: "))
+        subtract(x,y)
 
-elif user_choice == 'q':
-    print("Thanks for using my awesome calc app!!")
+    elif user_choice == 'm':
+        x = int(input("Enter the first number: "))
+        y = int(input("Enter the second number: "))
+        multiply(x,y)
 
-else:
-    print("Invalid input. Please try again.")  
+    elif user_choice == 'd':
+        x = int(input("Enter the first number: "))
+        y = int(input("Enter the second number: "))
+        divide(x,y)
+
+    elif user_choice == 'q':
+        print("Thanks for using my awesome calc app!!")
+        break
+
+    else:
+        print("Invalid input. Please try again.")
+
+  
 
